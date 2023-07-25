79c3bff4f43fa2b9eedb090889e192f4efad0c8f
kengiiGMs <kaikiaoto22@gmail.com>
2023-07-25 14:12:25 -0300

    Iniciando o Backend do Projeto

---
+++ backend/src/server.ts
@@ -0,0 +1,7 @@
+import express, { Response, Request, NextFunction } from "express";
+import { router } from "./routes";
+
+const app = express();
+app.use(express.json())
+app.use(router)
+app.listen(3333, () => console.log('Servidor online!!!'))
