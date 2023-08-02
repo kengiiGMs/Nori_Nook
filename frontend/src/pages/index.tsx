42a30a2d53b823ea232b867478b4219bcc4c5be2
kengiiGMs <kaikiaoto22@gmail.com>
2023-08-02 17:35:51 -0300

    Criando o contexto + Coletando os dados do login

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useContext, FormEvent, useState } from 'react'
 import Head from "next/head"
 import Image from "next/image"
 import styles from '../styles/home.module.scss'
@@ -7,9 +8,27 @@ import logoImg from '../../public/logo.svg'
 import { Input } from '../components/ui/Input/index'
 import { Button } from '../components/ui/Button/index'
 
+import { AuthContext } from '../contexts/AuthContext'
+
 import Link from 'next/link'
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext)
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [loading, setLoading] = useState(false)
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+    let data = {
+      email,
+      password
+    }
+    await signIn(data)
+  }
+
   return (
     <>
       <Head>
@@ -18,9 +37,11 @@ export default function Home() {
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo NoriNook" />
         <div className={styles.login}>
-          <form >
-            <Input placeholder="Digite o seu Email" type="text" />
-            <Input placeholder="Digite a sua Senha" type="password" />
+          <form onSubmit={handleLogin}>
+            <Input placeholder="Digite o seu Email" type="text" value={email}
+              onChange={(e) => setEmail(e.target.value)} />
+            <Input placeholder="Digite a sua Senha" type="password" value={password}
+              onChange={(e) => setPassword(e.target.value)} />
             <Button type="submit" loading={false}>
               Acessar
             </Button>
