5c7e66b783a04a0fcd8a42a6153ad8c410d3d664
kengiiGMs <kaikiaoto22@gmail.com>
2023-08-02 11:55:01 -0300

    Criando a tela de Login + Criando o Componente Input

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,27 @@
+import Head from "next/head"
+import Image from "next/image"
+import styles from '../styles/home.module.scss'
+
+import logoImg from '../../public/logo.svg'
+
+import { Input } from '../components/ui/input/index'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Nori Nook</h1>
+    <>
+      <Head>
+        <title>Nori Nook | Faça o seu Login</title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo NoriNook" />
+        <div className={styles.login}>
+          <form >
+            <Input placeholder="Digite o seu Email" type="text" />
+            <Input placeholder="Digite a sua Senha" type="password" />
 
-    </div>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
