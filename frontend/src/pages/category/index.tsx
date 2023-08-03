a58e314070d85548b3f74d5895ba2615e30dd365
kengiiGMs <kaikiaoto22@gmail.com>
2023-08-03 12:14:44 -0300

    Cadastrando novas categorias

--- frontend/src/pages/category/index.tsx
+++ frontend/src/pages/category/index.tsx
@@ -3,12 +3,27 @@ import { useState, FormEvent } from 'react'
 import Head from "next/head"
 import { Header } from '../../components/Header'
 import styles from './styles.module.scss'
+import { toast } from 'react-toastify'
+import { setupAPIClient } from '../../services/api'
+
 export default function Category() {
     const [name, setName] = useState('')
 
     async function handleRegister(event: FormEvent) {
         event.preventDefault();
 
+        if (name === '') {
+            toast.info("Preencha Todos os Campos!")
+            return;
+        }
+
+        const apiClient = setupAPIClient()
+        await apiClient.post('/category', {
+            name: name
+        })
+
+        toast.success("Categoria Cadastrada!")
+        setName('')
 
     }
 
