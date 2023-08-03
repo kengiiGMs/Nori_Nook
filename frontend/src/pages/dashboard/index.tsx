6e553b7729ae7fa9630bb96cd02a2123aa783bf8
kengiiGMs <kaikiaoto22@gmail.com>
2023-08-03 11:12:08 -0300

    Configurando rotas privadas

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,5 +1,16 @@
+import { canSSRAuth } from "../../utils/canSSRAuth"
+
+
 export default function Dashboard() {
     return (
         <h1>Bem vindo ao Painel</h1>
     )
 }
+
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+    return {
+        props: {}
+    }
+})
+
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -13,6 +13,7 @@ import { AuthContext } from '../contexts/AuthContext'
 import { toast } from 'react-toastify'
 
 import Link from 'next/link'
+import { canSSRGuest } from '../utils/canSSRGuest'
 
 export default function Home() {
   const { signIn } = useContext(AuthContext)
@@ -66,3 +67,9 @@ export default function Home() {
     </>
   )
 }
+
+export const getServerSideProps = canSSRGuest(async (ctx) => {
+  return {
+    props: {}
+  }
+})
