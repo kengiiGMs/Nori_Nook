b7ac1758ebe46a6c9a444afa3494754d0cb37ad4
kengiiGMs <kaikiaoto22@gmail.com>
2023-12-01 22:34:10 -0300

    Estilizando Página de Login

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,10 +1,61 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
 
 export default function SignIn() {
     return (
-        <View>
-            <Text>Tela de Login</Text>
+        <View style={styles.container}>
+            <Image style={styles.logo} source={require('../../assets/Logo.png')} />
+
+            <View style={styles.inputContainer}>
+                <TextInput placeholder='Digite o seu Email' style={styles.input} />
+                <TextInput placeholder='Digite a sua Senha' style={styles.input} secureTextEntry={true} />
+
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Acessar</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white'
+    },
+    logo: {
+        marginBottom: 18
+    },
+    inputContainer: {
+        width: '95%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 32,
+        paddingHorizontal: 14,
+    },
+    input: {
+        width: '95%',
+        height: 40,
+        backgroundColor: 'whitesmoke',
+        marginBottom: 12,
+        borderRadius: 4,
+        paddingHorizontal: 8,
+        color: 'black'
+    },
+    button: {
+        width: '95%',
+        height: 40,
+        backgroundColor: '#FF3F4B',
+        borderRadius: 4,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'white'
+    }
+
+})
--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 
 import AppRoutes from "./app.routes";
 import AuthRoutes from "./auth.routes";
@@ -9,6 +9,20 @@ function Routes() {
     const isAuthenticated = false;
     const loading = false;
 
+    if (loading) {
+        return (
+            <View style={{
+                flex: 1,
+                backgroundColor: 'whitesmoke',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+
+                <ActivityIndicator size={60} color="#FF3F4B" />
+
+            </View>
+        )
+    }
 
     return (
         isAuthenticated ? <AppRoutes /> : <AuthRoutes />
