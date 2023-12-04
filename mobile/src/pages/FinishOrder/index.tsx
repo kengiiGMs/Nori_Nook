0eb7bfd21bad75a276522c24d33443a889de4264
kengiiGMs <kaikiaoto22@gmail.com>
2023-12-04 18:17:51 -0300

    Finalizando Estrutura de Fechar Pedido

--- mobile/src/pages/FinishOrder/index.tsx
+++ mobile/src/pages/FinishOrder/index.tsx
@@ -1,14 +1,44 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { Feather } from '@expo/vector-icons'
+import { useNavigation, useRoute, RouteProp } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { StackParamsList } from "../../routes/app.routes";
+
+import { api } from '../../services/api'
+
+type RouteDetailParams = {
+    FinishOrder: {
+        number: string | number;
+        order_id: string;
+    }
+}
+
+type FinishOrderRouteProp = RouteProp<RouteDetailParams, 'FinishOrder'>
+
 
 export function FinishOrder() {
+    const route = useRoute<FinishOrderRouteProp>();
+    const navigation = useNavigation<NativeStackNavigationProp<StackParamsList>>();
+
+    async function handleFinish() {
+        try {
+            await api.put('order/send', {
+                order_id: route.params?.order_id
+            })
+
+            navigation.popToTop();
+        } catch (err) {
+            console.log("Erro ao Finalizar, Tente Mais Tarde")
+        }
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.alert}>Você Deseja Finaliza esse Pedido?</Text>
-            <Text style={styles.title}>Mesa 30</Text>
+            <Text style={styles.title}>Mesa {route.params?.number}</Text>
 
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button} onPress={handleFinish}>
                 <Text style={styles.textButton}>Finalizar Pedido</Text>
                 <Feather size={20} name="shopping-cart" color="white" />
 
