05c7bf886f059160172d2d05258f75ac6e230f54
kengiiGMs <kaikiaoto22@gmail.com>
2023-12-04 17:32:22 -0300

    Estrutura para Remover Itens do Pedido

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -115,6 +115,21 @@ export default function Order() {
         setItems(oldArray => [...oldArray, data])
     }
 
+    async function handleDeleteItem(item_id: string) {
+        await api.delete('/order/remove', {
+            params: {
+                item_id: item_id
+            }
+        })
+
+        let removeItem = items.filter(item => {
+            return (item.id !== item_id)
+        })
+
+        setItems(removeItem);
+
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -153,7 +168,7 @@ export default function Order() {
                 </TouchableOpacity>
             </View>
 
-            <FlatList showsVerticalScrollIndicator={false} style={{ flex: 1, marginTop: 24 }} data={items} keyExtractor={(item) => item.id} renderItem={({ item }) => <ListItem data={item} />} />
+            <FlatList showsVerticalScrollIndicator={false} style={{ flex: 1, marginTop: 24 }} data={items} keyExtractor={(item) => item.id} renderItem={({ item }) => <ListItem data={item} deleteItem={handleDeleteItem} />} />
 
             <Modal transparent={true} visible={modalCategoryVisible} animationType="fade">
                 <ModalPicker handleCloseModal={() => setModalCategoryVisible(false)} options={category} selectedItem={handleChangeCategory} />
